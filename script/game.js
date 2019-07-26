a457541edeab9d265c8f88f08ebd038f4e1bfd06
Vlad Serikov <vladikserikov@gmail.com>
2019-07-26 18:30:33 +0700

    new js

---
+++ script/game.js
@@ -0,0 +1,7 @@
+function playRound(e) {
+    // console.log(e.target)
+    const 
+}
+
+const hands = document.querySelectorAll('.block');
+hands.forEach(hand => hand.addEventListener('click', playRound))
