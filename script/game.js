22c4d3a355ad0ddd55ae8a3a46c07400a37b188b
Vlad Serikov <vladikserikov@gmail.com>
2019-08-03 06:20:23 +0700

    error property in attribute

--- script/game.js
+++ script/game.js
@@ -1,13 +1,66 @@
-function playRound(e) {
-    const elem = e.target.getAttribute('alt');
+function playRound(player, computer) {
+    // const computer = hands[computerSelection];
+    // const player = playerSelection.toLowerCase();
+    if (computer === player) {
+        return 0;
+    }
+    if (player == 'rock') {
+        return computer === 'paper' ? -1 : 1;
+    }
+    if (player == 'paper') {
+        return computer === 'rock' ? 1 : -1;
+    }
+    if (player == 'scissors') {
+        return computer === 'rock' ? -1 : 1;
+    }
 }
-let computer = 0,
-    dawn = 0
-    player = 0;
-const hands = document.querySelectorAll('.block');
+
+function computerPlay(hands) {
+    return hands[Math.floor(Math.random() * 3)]
+}
+
+function playRounds(playerSelection) {
+    const hands = ['rock', 'paper', 'scissors'];
+
+    let computerSelection = computerPlay(hands);
+
+    let result = playRound(playerSelection, computerSelection);
+    if (result == 0) {
+        dawnCount++;
+    } else if (result == 1) {
+        playerCount++;
+    } else {
+        computerCount++;
+    }
+
+}
+
+function playRound(e) {
+    console.log(e.target);
+    
+    if(e.target.hasAttribute('alt')){
+        let elem = e.target.getAttribute('alt');
+    } else{
+        return ;
+    }
+
+    
+    
+
+    playRounds(e.target.getAttribute('alt'));
+    console.log(`Player ${playerCount}\t Comp ${computerCount}\t Dawn ${dawnCount}`)
+}
+let computerCount = 0,
+    dawnCount = 0,
+    playerCount = 0;
 
 
-hands.forEach(hand => hand.addEventListener('click', playRound))
+const allHands = Array.from(document.querySelectorAll('.block'));
+allHands.forEach(hand => hand.addEventListener('click', playRound));    
+
+
+
+
 /* 
 function playRound(playerSelection, computerSelection) {
     const computer = hands[computerSelection];
